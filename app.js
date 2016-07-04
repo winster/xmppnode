3b3c63866a429ec62ed1584e252e73eb60d8af39
winster <wtjose@gmail.com>
2016-07-04 21:07:33 +0530

    Update app.js
    call to flask message rest api

--- app.js
+++ app.js
@@ -1,24 +1,50 @@
 var express = require('express');
 var http = require('http');
+var bodyParser = require('body-parser');
 var app = express();
+app.use(bodyParser.json());
 var GCM = require('node-gcm-ccs');
 var gcm = GCM('562766010128', 'AIzaSyBUEVVKBM3L93C92QMKDY7lfEEzNYTQuas');
  
 gcm.on('message', function(messageId, from, category, data) {
-	console.log('received message', arguments);
+    data.from = from
+    var post_data = JSON.stringify(data)
+    var post_options = {
+        host: 'surcle.herokuapp.com/v1.0',
+        port: '443',
+        path: '/message',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': Buffer.byteLength(post_data)
+        }
+    };
+    var post_req = http.request(post_options, (res) => {
+    	var json = '';
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => {
+            console.log('Response: ' + chunk);
+            json+=chunk
+        });
+        res.on('end', () => {
+            json=JSON.parse(json)
+            gcm.send(json.to, { message: json.message }, { delivery_receipt_requested: true }, (err, messageId, to) => {
+                if (!err) {
+                    console.log('sent message to', to, 'with message_id =', messageId);
+                } else {
+                    console.log('failed to send message');
+                }
+            })
+        });
+    });
+    post_req.write(post_data);
+    post_req.end();
+    console.log('received message', arguments);
 });
  
 gcm.on('receipt', function(messageId, from, category, data) {
-	console.log('received receipt', arguments);
+    console.log('received receipt', arguments);
 });
- 
-gcm.send('e3zFrvuRPeo:APA91bHHmsZXAjvsXD-fvnEnjh-fKqLAWStbHf3GXmIKIESKDmBQ_Hcm4WTteHtyQNVuRHyaBkaeAhiSvbRG-RrEoPW49Yd_2-Cmdli-qMcHI5Pg5vCqkIkbSvUc3ruN1uGTZU8U_B9P', { message: 'hello world' }, { delivery_receipt_requested: true }, function(err, messageId, to) {
-	if (!err) {
-		console.log('sent message to', to, 'with message_id =', messageId);
-	} else {
-		console.log('failed to send message');
-	}
-})
 
 var server = http.createServer(app)
 server.listen(process.env.PORT || 5000)
