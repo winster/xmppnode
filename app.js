8506ab3b9a758c3a84156e379e71016ffaa8b9cb
Winster-Thuruthippillil JOSE <wjose@amadeus.com>
2016-07-15 15:26:21 +0530

    websocket implementation - issues fixed

--- app.js
+++ app.js
@@ -3,17 +3,16 @@ var express = require('express'),
     http = require('http'),    
     bodyParser = require('body-parser'),
     GCM = require('node-gcm-ccs'),
-    WebSocketServer = require("ws").Server;
+    WebSocketServer = require("ws").Server,
+    shortid = require('shortid');
 
 var app = express();
 app.use(bodyParser.json());
 var gcm = GCM('415730801579', 'AIzaSyCt-ul4GBpRr2-F0tnp4HwYAWGTO8pimLo');
  
 gcm.on('message', function(messageId, from, category, data) {
-    //var data = {"to":"driver@amadeus.com","from":"devi@amadeus.com", "message": {"type":"text","data":"hello"}}
-    //var messageId = '12121212'
-    //var from = 'cVIpH6npR3w:APA91bEYPMzCBOjALL1oRHBJOJ0nmXPDLvbZlJGHtyLx4cZuhwfD00iQTC3h0peL4gz0ZmtH9Ku4mlZ9MvS_CVkrsocycNWPVHP3XhBpY94CjczmlLSrrSGR-DAavaJOb8WQf2IXBVQA'
     console.log('message received::'+JSON.stringify(data))
+    //Using Websocket instead of this event
 });
  
 gcm.on('receipt', function(messageId, from, category, data) {
@@ -37,45 +36,60 @@ server.listen(process.env.PORT || 5000, function () {
     console.log('Listening on ' + server.address().port) 
 });
 
+var clients = {}
+
 wss.on("connection", function(ws) {
-    var result = {'status':'connected'}
-    ws.send(JSON.stringify(result), function() {  })
+    var connection_id = shortid.generate();
+    clients[connection_id] = ws;
+    ws.connection_id = connection_id;
     console.log("websocket connection open");
+    var result = {'connection_id': connection_id}
+    ws.send(JSON.stringify(result), function() {  })
     ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-        try{
-            message = JSON.parse(message);
-        } catch(e){
-            message={"text":"send message in json format"};
-        }
-        var post_data = JSON.stringify(message)
-        var auth = "Basic " + new Buffer(message.user + ":" + message.from).toString("base64");
+        console.log('received: %s', message);
+        post_data = message;
+        message = JSON.parse(message);
+        var auth = "Basic " + new Buffer(message.user + ":" + message.token).toString("base64");
         post_options.headers['Authorization'] = auth;
         post_options.headers['Content-Length'] = Buffer.byteLength(post_data);
-        var post_req = https.request(post_options, (res) => {
+        var post_req = http.request(post_options, (res) => {
             var json = '';
             res.setEncoding('utf8');
             res.on('data', (chunk) => {
                 json+=chunk
             });
             res.on('end', () => {
-                //console.log('Response: ' + json);
-                json=JSON.parse(json)
-                var payload = {'user':message.user, 'message': message.content, 'id':message.messageId, 'time': message.time}
-                gcm.send(json.token, payload, { delivery_receipt_requested: true }, (err, messageId, to) => {
-                    if (!err) {
-                        console.log('sent message to', to, 'with message_id =', messageId);
+                console.log(json)
+                user=JSON.parse(json)
+                if(user.error){
+                    ws.send(json, function() {  });
+                    return;
+                }
+                var payload = {'user':message.user, 'data': message.data, 'id':message.id, 'time': new Date().getTime()}
+                if(user.online) {
+                    if(user.connection_id) {
+                        to_ws = clients[user.connection_id]
+                        to_ws.send(JSON.stringify(payload), function() {  })
                     } else {
-                        console.log('failed to send message');
+                        console.error('connection_id missing for user: %s not sending any message', message.to);
                     }
-                })
+                } else {
+                    gcm.send(user.token, payload, { delivery_receipt_requested: true }, (err, message.id, to) => {
+                        if (!err) {
+                            console.log('sent message to', to, 'with message_id =', message.id);
+                        } else {
+                            console.log('failed to send message');
+                        }
+                    })
+                }
             });
         });
         post_req.write(post_data);
-        post_req.end();    
+        post_req.end();
     });
     ws.on("close", function() {
-        console.log("websocket connection close");    
+        delete clients[ws.connection_id];
+        console.log("websocket connection closed ::", Object.keys(clients));            
     });
 });
 
